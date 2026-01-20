dbfb8c4dbeb5966c3c63cfe0668d8f84862091c7
exhorte <exhortemboumbaba@gmail.com>
2026-01-20 20:07:58 +0100

    crud-springboot

---
+++ PersonRepository.java
@@ -0,0 +1,8 @@
+package com.example.crud_springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
